3dae9572d0ef52318c057e772f9803bdceeffcca
IGNATIUS LIYAN <ignatiusliyan@gmail.com>
2024-09-04 19:29:43 +0530

    Google Map print statement

--- lib/mapGoogle.dart
+++ lib/mapGoogle.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class Mapgoogle extends StatefulWidget {
   const Mapgoogle({super.key});
@@ -10,12 +12,20 @@ class Mapgoogle extends StatefulWidget {
 
 class _MapgoogleState extends State<Mapgoogle> {
 
-  GoogleMapController? myController;
+  MapController _mapController = MapController();
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  double lantitude = 12.8859036;
+  double longtitude = 80.1986642;
 
-  void _onMapCreated(GoogleMapController controller) {
-    myController = controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // Perform actions with MapController here
+      _mapController.move(LatLng(lantitude,longtitude), 13);
+    });
+    print('');
   }
 
   @override
@@ -24,13 +34,35 @@ class _MapgoogleState extends State<Mapgoogle> {
       appBar: AppBar(
         title: Text('Map'),
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 10.0,
-        ),
-      ),
+      body: FlutterMap(
+              mapController:_mapController,
+              options: MapOptions(
+                initialCenter: LatLng(lantitude,longtitude),
+                onTap: (tapPosition,latlan){
+                  setState(() {
+                    lantitude=latlan.latitude;
+                    longtitude=latlan.longitude;
+                    _mapController.move(LatLng(lantitude,longtitude),13.0);
+                  });
+                },// Coordinates for San Francisco
+                minZoom: 13.0,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: LatLng(lantitude,longtitude),
+                      child: Icon(Icons.location_on,color: Colors.red,size: 40,),
+                    ),
+                  ],
+                ),
+              ],
+            ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import firebase_analytics
 import firebase_auth
 import firebase_core
+import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
